2f6a7ce55d88979fb6578d59830acacea8504414
Paolo Holinski <paolo@nextjournal.com>
2023-02-14 14:43:32 +0100

    Revert "Use babashka.http-client"
    This reverts commit ef38846f91db59aac24ac0c04a4be96d0fc16225.

--- src/nextjournal/cas_client.clj
+++ src/nextjournal/cas_client.clj
@@ -5,7 +5,7 @@
             [nextjournal.cas-client.hashing :as h]
             [cheshire.core :as json]
             [babashka.fs :as fs]
-            [babashka.http-client :as http]
+            [org.httpkit.client :as http]
             [org.httpkit.sni-client :as sni-client]
             [ring.util.codec :as ring-codec]))
 
@@ -19,22 +19,22 @@
 (defn tag-put [{:keys [host auth-token namespace tag target async]
                 :or {host *tags-host*
                      async false}}]
-  (http/post (str  host "/" namespace "/" tag)
-             {:headers {"auth-token" auth-token
-                        "content-type" "plain/text"}
-              :body target
-              :async async}))
+  (cond-> (http/post (str  host "/" namespace "/" tag)
+                     {:headers {"auth-token" auth-token
+                                "content-type" "plain/text"}
+                      :body target})
+    (not async) (deref)))
 
 (defn tag-url [{:keys [host namespace tag path]
                 :or {host *tags-host*}}]
   (str host "/" namespace "/" tag (when path (str "/" path))))
 
 (defn tag-get [opts]
-  (-> (http/get (tag-url opts))
+  (-> @(http/get (tag-url opts))
       :body))
 
 (defn tag-exists? [opts]
-  (-> (http/head (tag-url opts))
+  (-> @(http/head (tag-url opts))
       :status
       (= 200)))
 
@@ -47,16 +47,16 @@
            (str "?" query-params)))))
 
 (defn cas-exists? [opts]
-  (-> (http/head (cas-url opts))
+  (-> @(http/head (cas-url opts))
       :status
       (= 200)))
 
 (defn cas-get [opts]
-  (-> (http/get (cas-url opts))
+  (-> @(http/get (cas-url opts))
       :body
       slurp))
 
-(defn cas-put [{:keys [path host auth-token namespace tag]
+(defn cas-put [{:keys [path host auth-token namespace tag async]
                 :or {host *cas-host*
                      async false}}]
   (let [f (io/file path)
@@ -80,15 +80,18 @@
                                   :filename filename
                                   :content-type "application/clerk-cas-hash"
                                   :content hash}) files-already-uploaded))
-        {:as res :keys [status body]} (http/post
-                                       host
-                                       (cond-> {:multipart multipart}
-                                         tag (merge {:query-params {:tag (str namespace "/" tag)}
-                                                     :headers {"auth-token" auth-token}})))]
-    (if (= 200 status)
-      (-> body
-          (json/parse-string))
-      res)))
+        res (fn [] (let [{:as res :keys [status body]} @(http/post
+                                                         host
+                                                         (cond-> {:multipart multipart}
+                                                           tag (merge {:query-params {:tag (str namespace "/" tag)}
+                                                                       :headers {"auth-token" auth-token}})))]
+                     (if (= 200 status)
+                       (-> body
+                           (json/parse-string))
+                       res)))]
+    (if async
+      (future (res))
+      (res))))
 
 (defn put [{:as opts
             :keys [cas-host tags-host target path]
@@ -111,9 +114,9 @@
   (def r (put {:path "test/resources/foo"}))
   (def r (put {:cas-host "http://cas.dev.clerk.garden:8090"
                :tags-host "http://storage.dev.clerk.garden:8090"
-               :path "test/resources/foo"
+               :path "test/resources/foo.bak"
                :namespace "Sohalt"
                :tag "test-tag"
-               :auth-token (System/getenv "GITHUB_TOKEN")}))
-  (def m (r "manifest"))
+               :auth-token "foo" #_(System/getenv "GITHUB_TOKEN")}))
+  (def m (get r "manifest"))
   (def k (get m "bar/baz.txt")))
